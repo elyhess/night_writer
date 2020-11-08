495a45bba60e6b742de5996d9d0846345b65db42
Ely Hess <ely.hess@me.com>
2020-11-07 23:26:38 -0700

    Add writer class

---
+++ lib/writer.rb
@@ -0,0 +1,12 @@
+require_relative './alphabet'
+
+class Writer
+  attr_reader :imported_text, :output, :alpha
+  
+  def initialize(input, output)
+    @imported_text = input 
+    @output = output
+    @alpha = Alphabet.key
+  end
+
+end
