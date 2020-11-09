daaf84e3a040a308b7ca808905a506573d635be4
Ely Hess <ely.hess@me.com>
2020-11-09 10:37:58 -0700

    Delete new_line method & add module support

--- lib/writer.rb
+++ lib/writer.rb
@@ -19,12 +19,6 @@ class Writer
     imported_text.length > 40 
   end
 
-  def new_line(imported_text)
-    imported_text.chars.each_slice(40).map do |array|
-      array.join
-    end
-  end
-
   def convert_to_braille(line)
     line.each_char.reduce([]) do |braille, letter|
       if @alpha[letter] == @alpha[letter.downcase]
@@ -42,7 +36,7 @@ class Writer
   end
 
   def english_to_braille
-    new_line(imported_text).reduce("") do |translation, line|
+    new_line(imported_text, 40).reduce("") do |translation, line|
       if too_long?(imported_text)
         translation += (format_braille(convert_to_braille(line)) + "\n")
       else 
