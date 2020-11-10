29d47860355521b4c2cf78809cb1e131fca1b1ed
Ely Hess <ely.hess@me.com>
2020-11-10 14:39:39 -0700

    Add comment w/ run cmd

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,5 +1,5 @@
 require_relative './reader'
-
+# ruby ./lib/night_reader.rb braille.txt original_message.txt
 read_file = File.readlines(ARGV[0].chomp)
 night_reader = Reader.new(read_file, ARGV[1])
 night_reader.export
--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,5 +1,5 @@
 require_relative './writer'
-
+# ruby ./lib/night_writer.rb message.txt braille.txt
 read_file = File.read(ARGV[0].chomp)
 night_writer = Writer.new(read_file, ARGV[1])
 night_writer.export
